16b86f246db9984c5cc088e302906981987d255e
Killian Mannarelli <83612329+killian-mannarelli@users.noreply.github.com>
2021-12-06 21:27:36 +0100

    Api : Weather request  (#13)
    * getWeather fonction qui fetch accuweather
    
    * Fix, mise au norme du code

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const TBM_URL = "https://ws.infotbm.com/ws/1.0/get-realtime-pass";
-const WEATHER_URL = "";
+const WEATHER_URL = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/131928?apikey=SI5CGkV2GkHfHZARLw9La9DIYQchBu2F&language=fr-fr";
 const HYPPERPLANNING_URL = "";
 
 export const fetchTBM = async (stopId, lineId) => {
@@ -10,9 +10,16 @@ export const fetchTBM = async (stopId, lineId) => {
     throw `Erreur de récupération des données TBM (ligne: ${lineId}, arrêt: ${stopId}) : ${e}`;
   }
 };
-
+//Fetch accuweather qui renvoit un JSON contenant la météo du jour, on s'intéresse surtout ici au temps
 export const fetchWeather = async () => {
-  // TO DO
+  try {
+    const result = await fetch(WEATHER_URL);
+    const data =  await result.json();
+    return data.Headline.Category;
+  } catch (e) {
+    throw `Erreur de récupération des données météo : ${e}`;
+  }
+
 };
 
 export const fetchHyperplanning = async () => {
