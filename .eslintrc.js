0351ae22c7027ca618c1e3555d6a49b9fdd523e1
Riboulet <celian.riboulet@gmail.com>
2021-12-06 15:10:50 +0100

    ESLint init

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "no-trailing-spaces": 2,
+        "eol-last": 2,
+        "semi": 2,
+        "space-before-blocks": 2,
+        "arrow-spacing": [2, { "before": true, "after": true }],
+        "space-before-function-paren": [2, {
+            "anonymous": "always",
+            "named": "never",
+            "asyncArrow": "always"
+        }],
+        "template-curly-spacing": 2,
+        "eqeqeq": [2, "smart"],
+        "space-infix-ops": 2,
+        "quotes": [2, "double"],
+        "object-shorthand": 2,
+        "prefer-destructuring": [2, { "object": true, "array": false }],
+        "prefer-template": 2,
+    },
+};
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+    test() {
+      console.log("Hey");
+    },
+  },
+};
 </script>
 
 <style>
