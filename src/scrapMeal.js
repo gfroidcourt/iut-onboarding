388fef1a05c88b408b415f00cab0babd66a29b60
Killian Mannarelli <83612329+killian-mannarelli@users.noreply.github.com>
2021-12-21 16:02:06 +0100

    Récupération des menus CROUS (#28)
    * Remplissage du JSON ok, intégration à l'appli Vue en cours
    
    * Fix package lock
    
    * Fix package-lock.son, faire en sorte que EsLint valide le fichier
    
    * Fix ligne en trop ;)
    
    Co-authored-by: celian <celian.riboulet@gmail.com>

---
+++ src/scrapMeal.js
@@ -0,0 +1,55 @@
+
+import cheerio from "cheerio";
+import fetch from "node-fetch";
+
+const SPACE_URL = "https://www.crous-bordeaux.fr/restaurant/space-campus-resto-u/";
+const SIRTAKI_URL = "https://www.crous-bordeaux.fr/restaurant/crous-cafet-le-sirtaki/";
+
+/**
+ * Sélectionne tous les plats du menu du jour actuel, sans les entrées ni les desserts
+ * @param {string} URL
+ *  L'URL du site (space ou sirtaki)
+ * @return {string[]} un tableau de string correspondant aux plats du site pour le jour actuel
+ */
+async function fetchMenu(URL) {
+  const response = await fetch(URL);
+  const html = await response.text();
+
+  let $ = cheerio.load(html);
+  let tabPlats = [];
+  // Menu complet de chaque jour
+  $(".slides li").each((_, htmlDayMenu) => {
+
+    let dayMenu = $(htmlDayMenu).html();
+    // Sans les jours futurs
+    if (dayMenu.split(" ")[3] !== new Date().getDate().toString())
+      return;
+
+    $ = cheerio.load(dayMenu);
+    // Menu Crous du jour actuel
+    $(".liste-plats:nth-of-type(2)").each((_, htmlListMenu) => {
+      $ = cheerio.load(htmlListMenu);
+      // Plats du Crous du jour actuel
+      $("li").each((_, htmlPlat) => {
+        let plat = $(htmlPlat).html();
+        // Sans les lignes inutiles
+        if (plat.includes(":") || plat.includes("(") || plat.length === 0)
+          return;
+        // Sans les "Entrées diverses" et "Desserts divers"
+        if (plat.includes("Entrées") || plat.includes("Desserts"))
+          return;
+        tabPlats.push(plat);
+      });
+    });
+  });
+  return tabPlats;
+}
+
+/**
+ * Sélectionne les plats du jour actuel pour les deux restaurants crous
+ * @return Un objet contennant les plats des restaurants crous
+ */
+export const getAllRestaurantsMenus = async () => ({
+  space: await fetchMenu(SPACE_URL),
+  sirtaki: await fetchMenu(SIRTAKI_URL),
+});
