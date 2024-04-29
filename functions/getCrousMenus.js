3a2a99e828e0a165652ff282738f5aaa23a7467a
Xabi GOÏTY <xabigoity@gmail.com>
2024-04-29 17:27:02 +0200

    Started making smart menu display. //TODO: fix formatting problems with sirtaki

--- functions/getCrousMenus.js
+++ functions/getCrousMenus.js
@@ -35,9 +35,18 @@ async function fetchMenu(URL) {
     // Sans les lignes inutiles
     if (plat.includes(":") || plat.includes("(") || plat.length === 0)
       return;
-      // Sans les "Entrées diverses" et "Desserts divers"
+      // Sans les "Entrées diverses" et "Desserts divers" et sans les salades car on aime pas la salade
     if (plat.includes("Entrées") || plat.includes("Desserts") || plat.includes("Salade"))
       return;
+
+    //To remove **** in the sirtaki menu
+    plat = plat.replace('****','');
+    plat = plat.replace('****','');
+
+    if(plat.includes("Plat")) {
+      plat = plat.toUpperCase();
+    }
+
     tabPlats.push(plat.charAt(0).toUpperCase() + plat.slice(1));
   });
 
@@ -48,10 +57,32 @@ async function fetchMenu(URL) {
  * Sélectionne les plats du jour actuel pour les deux restaurants crous
  * @return Un objet contennant les plats des restaurants crous
  */
-const getAllRestaurantsMenus = async () => ({
-  space: await fetchMenu(SPACE_URL),
-  // sirtaki: await fetchMenu(SIRTAKI_URL),
-});
+const getAllRestaurantsMenus = async () => {
+  let sirtaki;
+  let space;
+  let sirtakiEnabled = true;
+  let spaceEnabled = true;
+  try {
+    sirtaki = fetchMenu(SIRTAKI_URL);
+  } catch(error) {
+    console.log("Unable to retreive Menu for Sirtaki. Error: "+error);
+    sirtakiEnabled = false;
+  }
+  console.log()
+  try {
+    space = fetchMenu(SPACE_URL);
+  } catch(error) {
+    console.log("Unable to retreive Menu for Space. Error: " + error);
+    spaceEnabled = false;
+  }
+  
+  return {
+    sirtakiEnabled: sirtakiEnabled,
+    spaceEnabled: spaceEnabled,
+    sirtaki: await fetchMenu(SIRTAKI_URL),
+    space: await fetchMenu(SPACE_URL),
+  };
+}
 
 export const handler = async () => {
   try {
--- src/views/Menus.vue
+++ src/views/Menus.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="view-container" v-show="isActive">
-    <h1 class="view-title">Menus de ce midi</h1>
+    <h1 class="view-title" id="MenuViewTitle">Menus de ce midi</h1>
     <div class="view-content">
       <MenuCard restName="SpaceCampus" :mealList="spaceMenu" />
+      <MenuCard restName="Sirtaki" :mealList="sirtakiMenu" />
     </div>
   </div>
 </template>
@@ -28,8 +29,17 @@ export default {
     api.getAllRestaurantsMenus().then((res) => {
       this.sirtakiMenu = res.sirtaki;
       this.spaceMenu = res.space;
+      if(!res.sirtakiEnabled) {
+        document.getElementsByClassName("view-content")[0].childNodes[1].style.display = "none";
+      }
+      if (!res.spaceEnabled) {
+        document.getElementsByClassName("view-content")[0].childNodes[0].style.display = "none";
+      }
+
+      if (!res.spaceEnabled && !res.sirtakiEnabled) {
+        document.getElementById("MenuViewTitle").innerHTML = "Erreur lors de la récupération des menus.";
+      }
     });
   },
 };
 </script>
-
