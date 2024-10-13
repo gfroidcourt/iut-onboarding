05f3b0502f7c2d763f28621d477c065b23536210
Xabi GOÏTY <xabigoity@gmail.com>
2024-10-13 16:42:01 +0200

    Re-enabled all necessary views

--- app.vue
+++ app.vue
@@ -129,34 +129,34 @@ export default {
             // 6h to 17h30
             const currentTime =
             new Date().getHours() * 60 + new Date().getMinutes();
-            return currentTime >= 6 * 60 && currentTime <= 17 * 60 + 30 && false;
+            return currentTime >= 6 * 60 && currentTime <= 17 * 60 + 30;
           }
         },
         transport: {
           time: () => DEVELOPEMENT_MODE ? 10000 : this.getTimeForBusesAndWeather(),
-          allowed: () => false,
+          allowed: () => true,
         },
         weather: {
           time: () => DEVELOPEMENT_MODE ? 10000 : 7 * 1000,
-          allowed: () => false,
+          allowed: () => true,
         },
         menus: {
           time: () => DEVELOPEMENT_MODE ? 10000 : 1000 * 15,
           allowed: () => {
             // 6h to 14h
             let currentHour = new Date().getHours();
-            return currentHour >= 6 && currentHour < 14 || true;
+            return currentHour >= 6 && currentHour < 14;
           },
         },
         /* Enable this at the start of each year (The QR code has to be updated)*/
         discord: {
           time: () => DEVELOPEMENT_MODE ? 10000 : 1000 * 30,
-          allowed: () => false,
+          allowed: () => true,
         },
         /* Enable when looking for new maintainers */
         maintainer: {
           time: () => DEVELOPEMENT_MODE ? 10000 : 1000 * 15,
-          allowed: () => false && !this.isEndOfDay(),
+          allowed: () => true && !this.isEndOfDay(),
         },
         announcement: {
           time: () => DEVELOPEMENT_MODE ? 10000 : 1000 * 15,
