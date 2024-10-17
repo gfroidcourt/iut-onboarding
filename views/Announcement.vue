9e694bc914df01e6230e1aaa6e37e14c36d95d58
Xabi GOÏTY <xabigoity@gmail.com>
2024-10-17 23:25:23 +0200

    Annonces Asso

--- views/Announcement.vue
+++ views/Announcement.vue
@@ -2,7 +2,8 @@
     <div class="view-container" v-show="isActive">
         <h1 class="view-title">Annonce de l'Assopeña</h1>
         <div class="announcment">
-            <img src="@/public/assets/Warning.png" id="AssoImg" />
+          <h2>Pour bien commencer la dernière semaine, le lundi 21/10/2024, le BDE vends des crêpes 0.5€ l'unité ! <br/> Une offre spéciale thé + 1 crêpe est disponible à 0.80€ !</h2>
+          <img src="@/public/assets/asso/assoCrepes.png" id="AssoImg" />
         </div>
     </div>
 </template>
@@ -15,8 +16,8 @@ export default{
 </script>
 <style>
 #AssoImg {
-    height: 625px;
-    width: 425px;
+    max-height: 425px;
+    max-width: 325px;
 }
 
 .announcment {
@@ -25,6 +26,12 @@ export default{
   box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.25);
   padding: 25px;
   display: flex;
-  flex-direction: row;
+  flex-direction: column !important;
+  align-items: center;
+}
+
+h2 {
+  font-family: Fredoka, sans-serif;
+  font-size: 35px;
 }
 </style>
